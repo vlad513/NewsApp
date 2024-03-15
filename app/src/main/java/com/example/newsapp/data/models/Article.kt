d68b34ba7cfd17c24458f82e8f49a7a81bafa008
vlad513 <78737484+vlad513@users.noreply.github.com>
2024-03-15 21:33:39 +0300

    Реализовал дизайн экрана DetailsFragment.kt настроил передачу данных чрез бандл , реализовал отображение данных

--- app/src/main/java/com/example/newsapp/data/models/Article.kt
+++ app/src/main/java/com/example/newsapp/data/models/Article.kt
@@ -6,13 +6,13 @@ import androidx.room.PrimaryKey
 @Entity(tableName = "article")
 data class Article(
     @PrimaryKey(autoGenerate = true)
-    val id:Int? = null,
-    val author: String,
-    val content: String,
-    val description: String,
-    val publishedAt: String,
-    val source: Source,
-    val title: String,
-    val url: String,
-    val urlToImage: String
-)
+    val id:Int = 0,
+    val author: String?,
+    val content: String?,
+    val description: String?,
+    val publishedAt: String?,
+    val source: Source?,
+    val title: String?,
+    val url: String?,
+    val urlToImage: String?
+):java.io.Serializable
--- app/src/main/java/com/example/newsapp/ui/details/DetailsFragment.kt
+++ app/src/main/java/com/example/newsapp/ui/details/DetailsFragment.kt
@@ -1,21 +1,63 @@
 package com.example.newsapp.ui.details
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.webkit.URLUtil
+import android.widget.Toast
+import androidx.core.content.ContextCompat
+import androidx.navigation.fragment.navArgs
+import com.bumptech.glide.Glide
 import com.example.newsapp.databinding.FragmentDetailsBinding
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.android.synthetic.main.fragment_details.*
 
-
+@AndroidEntryPoint
 class DetailsFragment : Fragment() {
 
     lateinit var binding: FragmentDetailsBinding
+    private val bundleArgs: DetailsFragmentArgs by navArgs()
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         binding = FragmentDetailsBinding.inflate(layoutInflater)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val articleArg = bundleArgs.article
+
+        articleArg.let { article ->
+            article.urlToImage.let {
+                Glide.with(this).load(article.urlToImage).into(header_image)
+            }
+            header_image.clipToOutline = true
+            article_details_title.text = article.title
+            article_details_decription_text.text = article.description
+
+            article_details_button.setOnClickListener {
+                try {
+                    Intent()
+                        .setAction(Intent.ACTION_VIEW)
+                        .addCategory(Intent.CATEGORY_BROWSABLE)
+                        .setData(Uri.parse(takeIf { URLUtil.isValidUrl(article.url) }
+                            ?.let {
+                                article.url } ?: "https://google.com"))
+                        .let {
+                            ContextCompat.startActivity(requireContext(), it, null)
+                        }
+
+                } catch (e: java.lang.Exception) {
+                    Toast.makeText(context, "The device doesn't have any browser to view the document!", Toast.LENGTH_SHORT).show()
+                }
+            }
+        }
     }
 }
